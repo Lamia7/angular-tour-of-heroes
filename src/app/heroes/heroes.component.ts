95def204cf28452fe1396449bb031898ea7486b4
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-01-19 15:22:45 +0100

    ✨ Display a list of hero with selectable option

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from '../hero';
+import { Hero } from '../models/hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
+  heroes: Hero[] = HEROES; // prop heroes de type classe Hero[] à qui on assigne HEROES:une constante qui a déjà une valeur (voir mock)
+  selectedHero: Hero; // prop selectedHero de type Hero (pas encore de valeur de départ)
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero; // affecte l'instance hero à selectedHero
   }
-
 }
