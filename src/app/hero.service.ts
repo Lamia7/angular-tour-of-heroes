2f269b54838f6c8c67223cc9cc33b30b9f66cab7
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-01-20 20:59:11 +0100

    🚧 HeroServ MsgServ and HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,13 +4,24 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './models/hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // URL to web api
+
   // créer une prop privée à la construction du fichier du hero service
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   // méthode qui renvoie les héros ici via un observable de la classe Hero qui est un tableau
   getHeroes(): Observable<Hero[]> {
