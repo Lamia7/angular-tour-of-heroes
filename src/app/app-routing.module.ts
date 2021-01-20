d4b761f5c60eae414eba4eb4689612677d4c0bf9
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-01-20 18:41:48 +0100

    🚧 Added 'default' + 'dashboard' routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // permet à l'app d'avoir fonctionnalité de routage
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component'; // donne au routeur un endroit où aller une fois les routes configurées
 
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // itinéraire par défaut
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
