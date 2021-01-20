c4dfa551667f4cd68e409a7abdaf19a3fc8b74d5
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-01-20 18:04:18 +0100

    🚧 Added router and 'heroes' route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router'; // permet à l'app d'avoir fonctionnalité de routage
+import { HeroesComponent } from './heroes/heroes.component'; // donne au routeur un endroit où aller une fois les routes configurées
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule], // routeur dispo ds l'app
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
