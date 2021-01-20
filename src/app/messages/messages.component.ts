305b7c2d4beb0f70b5ecfbd523504b3696a7bac2
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-01-20 14:40:11 +0100

    🚧 Display msg from HeroServ

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  // messageService publique car sera liée et dc visible dans le modèle
+  constructor(public messageService: MessageService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
