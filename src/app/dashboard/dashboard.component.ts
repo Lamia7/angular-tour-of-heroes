53f082c1396a491e87e364c662389d84a3ae8227
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-01-20 18:36:15 +0100

    🚧 Added Dashboard component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../models/hero';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes(); // appeler cette méthode à l'init du composant
+  }
+
+  // méthode qui récupère les héros du service de 2e à 5e position
+  getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
+  }
+}
