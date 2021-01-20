ab3eac3416b9810ba0c1dda544a8738f524ad34f
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-01-20 19:57:31 +0100

    🚧 Hero component links

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,22 +11,18 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[]; // simple déclaration: prop heroes de type classe Hero[]
-  selectedHero: Hero; // prop selectedHero de type Hero (pas encore de valeur de départ)
 
   // ajout d'un param privé de type HeroService au constructeur pr injecter le serv
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes(); // appeler cette méthode à l'init du composant
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero; // affecte l'instance hero à selectedHero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  // this.selectedHero = hero; // affecte l'instance hero à selectedHero
+  // this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
   // méthode qui récupère les héros du service
   getHeroes(): void {
